ae6cdda9b92c83caeb83287cc59a8d76645a4ca9
paulofranklins <paulofranklinc@gmail.com>
2025-07-10 16:06:01 -0700

    Initial commit

---
+++ src/test/java/com/paulofranklins/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paulofranklins.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
