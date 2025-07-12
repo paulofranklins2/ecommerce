d022493bf81290fa7c1eb9e7f63853bc00745d3e
paulofranklins <paulofranklinc@gmail.com>
2025-07-12 05:03:24 -0700

    refactor: add @RequiredArgsConstructor and @NonNull to Product for constructor generation and null safety

--- src/main/java/com/paulofranklins/ecommerce/model/Product.java
+++ src/main/java/com/paulofranklins/ecommerce/model/Product.java
@@ -2,6 +2,9 @@ package com.paulofranklins.ecommerce.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 import java.math.BigDecimal;
@@ -12,15 +15,18 @@ import static jakarta.persistence.GenerationType.IDENTITY;
 @Entity
 @Data
 @Table(name = "product")
+@RequiredArgsConstructor
+@NoArgsConstructor
 public class Product {
     @Id
     @GeneratedValue(strategy = IDENTITY)
     private Integer id;
-    private @NotBlank String name;
-    private @NotBlank String description;
-    private @NotBlank BigDecimal price;
-    private @NotBlank String imageUrl;
+    private @NonNull @NotBlank String name;
+    private @NonNull @NotBlank String description;
+    private @NonNull @NotBlank BigDecimal price;
+    private @NonNull @NotBlank String imageUrl;
     @ManyToOne(fetch = LAZY, optional = false)
     @JoinColumn(name = "category_id")
-    private @NotBlank Category category;
+    private @NonNull @NotBlank Category category;
+
 }
