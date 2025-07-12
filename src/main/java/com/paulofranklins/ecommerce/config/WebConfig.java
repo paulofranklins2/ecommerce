f72f00eb9d0fb9dddf8593d950768116046209d1
paulofranklins <paulofranklinc@gmail.com>
2025-07-12 05:05:17 -0700

    feat: add ProductRepository with findByName query method

--- src/main/java/com/paulofranklins/ecommerce/config/WebConfig.java
+++ src/main/java/com/paulofranklins/ecommerce/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.paulofranklins.ecommerce.config;
 
+import lombok.NonNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -12,7 +13,7 @@ public class WebConfig {
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(CorsRegistry registry) {
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE");
             }
         };
