50fbcbdbd4e749411761c99a58803197c50b65f0
paulofranklins <paulofranklinc@gmail.com>
2025-07-12 05:06:24 -0700

    feat: create ProductDto for data transfer with validation constraints

---
+++ src/main/java/com/paulofranklins/ecommerce/dto/ProductDto.java
@@ -0,0 +1,16 @@
+package com.paulofranklins.ecommerce.dto;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import java.math.BigDecimal;
+
+@Data
+public class ProductDto {
+    private Integer id;
+    private @NotBlank String name;
+    private @NotBlank String description;
+    private @NotBlank BigDecimal price;
+    private @NotBlank String imageUrl;
+    private @NotBlank Integer categoryId;
+}
