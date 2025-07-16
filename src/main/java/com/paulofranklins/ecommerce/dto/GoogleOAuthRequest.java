27dbbef93b87b888b10abd4116359191d0a4daa9
paulofranklins <paulofranklinc@gmail.com>
2025-07-16 15:23:46 -0700

    add google oauth service implementation

---
+++ src/main/java/com/paulofranklins/ecommerce/dto/GoogleOAuthRequest.java
@@ -0,0 +1,8 @@
+package com.paulofranklins.ecommerce.dto;
+
+import lombok.Data;
+
+@Data
+public class GoogleOAuthRequest {
+    private String code;
+}
---
+++ src/main/java/com/paulofranklins/ecommerce/service/GoogleOAuthService.java
@@ -0,0 +1,51 @@
+package com.paulofranklins.ecommerce.service;
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeTokenRequest;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import com.google.api.client.googleapis.auth.oauth2.GoogleTokenResponse;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.gson.GsonFactory;
+import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+public class GoogleOAuthService {
+
+    @Value("${google.client_id}")
+    private String clientId;
+
+    @Value("${google.client_secret}")
+    private String clientSecret;
+
+    @Value("${google.redirect_uri}")
+    private String redirectUri;
+
+    @SneakyThrows
+    public GoogleTokenResponse exchangeCodeForTokens(String code) {
+        return new GoogleAuthorizationCodeTokenRequest(
+                new NetHttpTransport(),
+                new GsonFactory(),
+                "https://oauth2.googleapis.com/token",
+                clientId,
+                clientSecret,
+                code,
+                redirectUri
+        ).execute();
+    }
+
+    @SneakyThrows
+    public GoogleIdToken.Payload verifyToken(String idTokenString) {
+        GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(new NetHttpTransport(), new GsonFactory()).setAudience(Collections.singletonList(clientId)).build();
+
+        GoogleIdToken idToken = verifier.verify(idTokenString);
+
+        if (idToken != null) return idToken.getPayload();
+        else throw new IllegalArgumentException("Invalid ID token");
+
+    }
+
+}
