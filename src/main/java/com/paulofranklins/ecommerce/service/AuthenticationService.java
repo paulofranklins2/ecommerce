3aaafcc67e9a853b471f8d72725062b36593b83a
paulofranklins <paulofranklinc@gmail.com>
2025-07-17 14:54:09 -0700

    feat: add user registration with random password

--- src/main/java/com/paulofranklins/ecommerce/service/AuthenticationService.java
+++ src/main/java/com/paulofranklins/ecommerce/service/AuthenticationService.java
@@ -1,5 +1,6 @@
 package com.paulofranklins.ecommerce.service;
 
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken.Payload;
 import com.paulofranklins.ecommerce.dto.user.LoginUserDto;
 import com.paulofranklins.ecommerce.dto.user.RegisterUserDto;
 import com.paulofranklins.ecommerce.model.User;
@@ -8,6 +9,8 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Random;
 
 @Service
 public class AuthenticationService {
@@ -21,7 +24,6 @@ public class AuthenticationService {
         this.authenticationManager = authenticationManager;
     }
 
-
     public User registerUser(RegisterUserDto registerUserDto) {
         User user = new User();
         user.setFirstName(registerUserDto.getFirstName());
@@ -34,6 +36,30 @@ public class AuthenticationService {
     public User authenticateUser(LoginUserDto loginUserDto) {
         authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginUserDto.getEmail(), loginUserDto.getPassword()));
         return userRepository.findByEmail(loginUserDto.getEmail()).orElseThrow();
+    }
+
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email).orElseThrow();
+    }
+
+    public User register(Payload payload) {
+        User user = new User();
+        user.setFirstName(payload.get("given_name").toString());
+        user.setLastName(payload.get("family_name").toString());
+        user.setEmail(payload.getEmail());
+        user.setPassword(passwordEncoder.encode(generateRandomPassword()));
+
+        return userRepository.save(user);
+    }
+
+    private static String generateRandomPassword() {
+        String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        StringBuilder password = new StringBuilder();
+        Random random = new Random();
+        for (int i = 0; i < 16; i++) {
+            password.append(chars.charAt(random.nextInt(chars.length())));
+        }
+        return password.toString();
     }
 
 }
