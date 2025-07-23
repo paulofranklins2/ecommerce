12148020e3a3ad148959b66b11575201cd2292eb
paulofranklins <paulofranklinc@gmail.com>
2025-07-23 16:36:44 -0700

    feat: add OrderRepository interface for Order persistence

---
+++ src/main/java/com/paulofranklins/ecommerce/repository/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.paulofranklins.ecommerce.repository;
+
+import com.paulofranklins.ecommerce.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Integer> {
+}
